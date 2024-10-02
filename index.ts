7ab936ecdcbd351ea1f36f585913ff119009d0c5
Shivarjun Bhogal <sbhogal@hcrlaw.com>
2024-10-02 16:52:44 +0100

    Created Types and Inteface

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+type StorageType = "local" | "session" | "hashmap";
+type StorageObject = IStorage;
+
+interface IStorage {
+    //setItem(key: string, value: string): void;
+    //getItem(key: string): string | null;
+    //removeItem(key: string): void;
+    //clear(): void;
+    //keyExists(key: string): boolean;
+    //keys(): string[];
+}
