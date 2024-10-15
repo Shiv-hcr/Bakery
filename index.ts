72220a83e3f0def4f71e12a33277e28022499ae7
Shivarjun Bhogal <sbhogal@hcrlaw.com>
2024-10-15 09:59:16 +0100

    Initial Upload

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 type StorageType = "local" | "session" | "hashmap";
 type StorageObject = IStorage;
+type Bucket = [string, string][];
 
 interface IStorage {
     //setItem(key: string, value: string): void;
@@ -8,4 +9,93 @@ interface IStorage {
     //clear(): void;
     //keyExists(key: string): boolean;
     //keys(): string[];
+}
+
+class Storage implements IStorage {
+
+    #storage: StorageObject;
+    #keyCache: Set<string> = new Set();
+
+    constructor(type: StorageType) {
+        this.#storage = this.#initialiseStorage(type);
+    }
+
+    #initialiseStorage(type: StorageType): StorageObject {
+        switch (type) {
+            case "local": 
+                //return new BrowserStorage(localStorage);
+            case "session":
+                //return new BrowserStorage(sessionStorage);
+            case "hashmap":
+                //return new Hashmap();
+            default:
+                console.warn(`Invalid storage type: "${type}". Defaulting to Hashmap`);
+                //return new Hashmap();
+        }
+    }
+}
+
+class Hashmap implements IStorage {
+    #size: number;
+    #buckets: Bucket[];
+    #count: number = 0;
+    #loadFactor: number = 0.7;
+    #initialSize: number = 4;
+
+    constructor(size?: number) {
+        this.#size = size || this.#initialSize;
+        this.#buckets = this.#new(this.#size);
+    }
+
+    #new(size: number): Bucket[] {
+        this.#count = 0;
+        return Array.from({ length: size }, () => []);
+    }
+
+    #resize(newSize: number): void {
+        const oldBuckets = this.#buckets;
+        this.#size = newSize;
+        this.#buckets = this.#new(newSize);
+        console.log(`Resizing hashmap to size ${newSize}...`);
+        for (const bucket of oldBuckets) {
+            for (const [key, value] of bucket) {
+                this.setItem(key, value);
+            }
+        }
+    }
+
+    #checkLoadFactor(): void {
+        const factorIncrease: number = 2;
+        const currentLoadFactor: number = this.#count / this.#size;
+        if (currentLoadFactor > this.#loadFactor) {
+            this.#resize(this.#size * factorIncrease);
+        }
+    }
+
+    #checkUnderLoadFactor(): void {
+        const factorDecrease: number = 0.5;
+        const underLoadFactor: number = 0.4;
+        const currentLoadFactor: number = this.#count / this.#size;
+
+        if (currentLoadFactor < underLoadFactor && this.#size > 10) {
+            this.#resize(Math.ceil(this.#size * factorDecrease));
+        }
+    }
+
+    #hash(key: string): number {
+        const FNV_PRIME: number = 16777619;
+        const FNV_OFFSET_BASIS: number = 2166136261;
+        let hash: number = FNV_OFFSET_BASIS;
+
+        for (let i = 0; i < key.length; i++) {
+            hash ^= key.charCodeAt(i);
+            hash *= FNV_PRIME;
+            hash = hash >>> 0; // Convert to 32-bit int
+        }
+        
+        return hash % this.#size;
+    }
+
+
+
 }
